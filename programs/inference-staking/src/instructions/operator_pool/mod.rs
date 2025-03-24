a0488aa90c5a09ba118cbf3e143026da06f7cbc7
Taylor123 <nftaylor11@gmail.com>
2025-03-24 17:57:35 -0400

    withdraw operator commissions

--- programs/inference-staking/src/instructions/operator_pool/mod.rs
+++ programs/inference-staking/src/instructions/operator_pool/mod.rs
@@ -1,5 +1,7 @@
 pub mod create_operator_pool;
 pub mod slash_stake;
+pub mod withdraw_operator_commission;
 
 pub use create_operator_pool::*;
 pub use slash_stake::*;
+pub use withdraw_operator_commission::*;
