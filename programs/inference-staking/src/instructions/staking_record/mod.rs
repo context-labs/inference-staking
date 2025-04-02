44ec8f7bdf98be088d2cbf0edbc012ba2dc8e541
iurage <iurage@protonmail.com>
2025-04-03 07:20:25 +0800

    Add ix

--- programs/inference-staking/src/instructions/staking_record/mod.rs
+++ programs/inference-staking/src/instructions/staking_record/mod.rs
@@ -1,9 +1,11 @@
+pub mod cancel_unstake;
 pub mod claim_unstake;
 pub mod close_staking_record;
 pub mod create_staking_record;
 pub mod stake;
 pub mod unstake;
 
+pub use cancel_unstake::*;
 pub use claim_unstake::*;
 pub use close_staking_record::*;
 pub use create_staking_record::*;
