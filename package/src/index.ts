dc2372b8ed30464f735ffd1ae3366cc36ffe701c
iurage <iurage@protonmail.com>
2025-04-06 16:09:39 +0800

    Refactor test utils

--- package/src/index.ts
+++ package/src/index.ts
@@ -1,11 +1,15 @@
 import { AnchorProvider, Program } from "@coral-xyz/anchor";
 import { IDL } from "./idl";
 
+export * from "./merkle";
+
 /**
  * Creates an Anchor program instance of the Inference.net staking program.
  * @param provider
  * @returns
  */
-export const createProgram = (provider: AnchorProvider): Program<typeof IDL> => {
+export const createProgram = (
+  provider: AnchorProvider
+): Program<typeof IDL> => {
   return new Program(IDL, provider);
 };
