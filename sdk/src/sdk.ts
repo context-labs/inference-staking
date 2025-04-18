c5b2c4e4af96d54c275db44e96872d97a8387288
Sean <sean@kuzco.xyz>
2025-04-17 18:27:47 -0700

    Add helper methods to SDK

--- sdk/src/sdk.ts
+++ sdk/src/sdk.ts
@@ -1,4 +1,5 @@
-import type { AnchorProvider, BN } from "@coral-xyz/anchor";
+import type { AnchorProvider } from "@coral-xyz/anchor";
+import { BN } from "@coral-xyz/anchor";
 import { Program, AnchorError, BorshCoder } from "@coral-xyz/anchor";
 import type {
   AccountMeta,
@@ -14,6 +15,10 @@ import type {
   InferenceStakingInstructions,
   InstructionArgsMap,
   InstructionAccountsMap,
+  OperatorPoolAccountStruct,
+  PoolOverviewAccountStruct,
+  RewardRecordAccountStruct,
+  StakingRecordAccountStruct,
 } from "./types";
 import type { SolanaEnvironment } from "./utils";
 import { getProgramIdFromEnvironment, capitalize, toCamelCase } from "./utils";
@@ -50,9 +55,13 @@ export class InferenceStakingProgramSDK {
     return pda;
   }
 
-  stakingRecordPda(operatorPoolPda: PublicKey): PublicKey {
+  stakingRecordPda(operatorPoolPda: PublicKey, owner: PublicKey): PublicKey {
     const [pda] = PublicKey.findProgramAddressSync(
-      [Buffer.from("StakingRecord", "utf-8"), operatorPoolPda.toBuffer()],
+      [
+        operatorPoolPda.toBuffer(),
+        owner.toBuffer(),
+        Buffer.from("StakingRecord", "utf-8"),
+      ],
       this.program.programId
     );
     return pda;
@@ -88,6 +97,139 @@ export class InferenceStakingProgramSDK {
       this.program.programId
     );
     return pda;
+  }
+
+  async fetchPoolOverview(): Promise<PoolOverviewAccountStruct> {
+    const poolOverviewPda = this.poolOverviewPda();
+    return this.program.account.poolOverview.fetch(poolOverviewPda);
+  }
+
+  async fetchOperatorPool(
+    operatorPoolId: BN
+  ): Promise<OperatorPoolAccountStruct> {
+    const operatorPoolPda = this.operatorPoolPda(operatorPoolId);
+    return this.program.account.operatorPool.fetch(operatorPoolPda);
+  }
+
+  async fetchOperatorPoolByAddress(
+    operatorPoolAddress: PublicKey
+  ): Promise<OperatorPoolAccountStruct> {
+    return this.program.account.operatorPool.fetch(operatorPoolAddress);
+  }
+
+  async fetchStakingRecord(
+    operatorPoolPda: PublicKey,
+    owner: PublicKey
+  ): Promise<StakingRecordAccountStruct> {
+    const stakingRecordPda = this.stakingRecordPda(operatorPoolPda, owner);
+    return this.program.account.stakingRecord.fetch(stakingRecordPda);
+  }
+
+  async fetchOperatorStakingRecord(
+    operatorPoolPda: PublicKey
+  ): Promise<StakingRecordAccountStruct> {
+    const operatorPool = await this.fetchOperatorPoolByAddress(operatorPoolPda);
+    return this.program.account.stakingRecord.fetch(
+      operatorPool.operatorStakingRecord
+    );
+  }
+
+  async fetchRewardRecord(epoch: BN): Promise<RewardRecordAccountStruct> {
+    const rewardRecordPda = this.rewardRecordPda(epoch);
+    return this.program.account.rewardRecord.fetch(rewardRecordPda);
+  }
+
+  async fetchAllOperatorPools(): Promise<
+    { publicKey: PublicKey; account: OperatorPoolAccountStruct }[]
+  > {
+    return this.program.account.operatorPool.all();
+  }
+
+  async fetchAllStakingRecordsForOwner(
+    owner: PublicKey
+  ): Promise<{ publicKey: PublicKey; account: StakingRecordAccountStruct }[]> {
+    return this.program.account.stakingRecord.all([
+      {
+        memcmp: {
+          offset: 8,
+          bytes: owner.toBase58(),
+        },
+      },
+    ]);
+  }
+
+  async fetchAllStakingRecordsForPool(
+    operatorPoolPda: PublicKey
+  ): Promise<{ publicKey: PublicKey; account: StakingRecordAccountStruct }[]> {
+    return this.program.account.stakingRecord.all([
+      {
+        memcmp: {
+          offset: 8 + 32,
+          bytes: operatorPoolPda.toBase58(),
+        },
+      },
+    ]);
+  }
+
+  async fetchAllRewardRecords(): Promise<
+    { publicKey: PublicKey; account: RewardRecordAccountStruct }[]
+  > {
+    return this.program.account.rewardRecord.all();
+  }
+
+  async fetchRewardRecordForEpoch(
+    epoch: BN
+  ): Promise<RewardRecordAccountStruct> {
+    const rewardRecordPda = this.rewardRecordPda(epoch);
+    return this.program.account.rewardRecord.fetch(rewardRecordPda);
+  }
+
+  async fetchLatestRewardRecord(): Promise<RewardRecordAccountStruct> {
+    const poolOverview = await this.fetchPoolOverview();
+    const latestEpoch = poolOverview.completedRewardEpoch;
+    return this.fetchRewardRecordForEpoch(latestEpoch);
+  }
+
+  async hasStakingRecord(
+    operatorPoolPda: PublicKey,
+    owner: PublicKey
+  ): Promise<boolean> {
+    try {
+      const stakingRecordPda = this.stakingRecordPda(operatorPoolPda, owner);
+      await this.program.account.stakingRecord.fetch(stakingRecordPda);
+      return true;
+    } catch {
+      return false;
+    }
+  }
+
+  async fetchStakedTokenAccountBalance(
+    operatorPoolPda: PublicKey
+  ): Promise<BN> {
+    const stakedTokenPda = this.stakedTokenPda(operatorPoolPda);
+    const tokenAccountInfo =
+      await this.program.provider.connection.getTokenAccountBalance(
+        stakedTokenPda
+      );
+    return new BN(tokenAccountInfo.value.amount);
+  }
+
+  async fetchFeeTokenAccountBalance(operatorPoolPda: PublicKey): Promise<BN> {
+    const feeTokenPda = this.feeTokenPda(operatorPoolPda);
+    const tokenAccountInfo =
+      await this.program.provider.connection.getTokenAccountBalance(
+        feeTokenPda
+      );
+    return new BN(tokenAccountInfo.value.amount);
+  }
+
+  async fetchRewardTokenAccountBalance(): Promise<BN> {
+    const rewardTokenPda = this.rewardTokenPda();
+    const tokenAccountInfo =
+      await this.program.provider.connection.getTokenAccountBalance(
+        rewardTokenPda
+      );
+    return new BN(tokenAccountInfo.value.amount);
   }
 
   static getErrorNameFromTransactionLogs(
