d0b4ca0c63e0b9c00e4232f3be7a024c2a9ee872
Sean <sean@kuzco.xyz>
2025-06-13 18:06:18 -0700

    Update devnet USDC mint

--- programs/inference-staking/src/constants.rs
+++ programs/inference-staking/src/constants.rs
@@ -4,7 +4,7 @@ use anchor_lang::prelude::*;
 pub const USDC_MINT_PUBKEY: Pubkey = pubkey!("usdEkK5GbzC22bd2gKMFpt6sY2YETm2eaCiu7bBheZV");
 
 #[cfg(feature = "devnet")]
-pub const USDC_MINT_PUBKEY: Pubkey = pubkey!("4zMMC9srt5Ri5X14GAgXhaHii3GnPAEERYPJgZJDncDU");
+pub const USDC_MINT_PUBKEY: Pubkey = pubkey!("usdEkK5GbzC22bd2gKMFpt6sY2YETm2eaCiu7bBheZV");
 
 #[cfg(all(not(feature = "test"), not(feature = "devnet")))]
 pub const USDC_MINT_PUBKEY: Pubkey = pubkey!("EPjFWdd5AufqSSqeM2qN1xzybapC8G4wEGGkZwyTDt1v");
